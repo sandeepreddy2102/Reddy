82edab052adbe737cd05e350ff61832f12ba3a09
sandeep <sandeepg2102@gmail.com>
2017-04-27 07:57:23 -0400

    spinner

--- app/src/main/java/com/abc/reddy/MainActivity.java
+++ app/src/main/java/com/abc/reddy/MainActivity.java
@@ -2,9 +2,12 @@ package com.abc.reddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Spinner spinnerl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
