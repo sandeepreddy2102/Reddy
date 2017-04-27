f531164d6db5de9e8b1bd817b482a8a26c290a0d
UmaAndroidNGAGrpInc <uma@excelsoftservices.com>
2017-04-27 07:49:38 -0400

    testing

--- app/src/main/java/com/abc/reddy/MainActivity.java
+++ app/src/main/java/com/abc/reddy/MainActivity.java
@@ -2,12 +2,15 @@ package com.abc.reddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
